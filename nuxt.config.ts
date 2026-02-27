b18a867257505457b422956c1ac39c56814d8142
Jice19 <2452881917@qq.com>
2026-02-27 19:55:31 +0800

    feat: 导入terser压缩丑化js产物

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,15 @@ export default defineNuxtConfig({
       autoprefixer: {}
     }
   },
+  vite: {
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }
+    }
+  }
 })
